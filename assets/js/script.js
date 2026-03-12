1c7ccef695e553d5bfbd3db6eca8193b29404601
NucleoDaguiar <nucleo.daguiar@gmail.com>
2026-03-11 21:01:00 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@
 
 const CONFIG = {
 
-WHATSAPP: "https://wa.me/5599999999999",
+WHATSAPP: "https://wa.me/5562991787997",
 
 INSTAGRAM: "https://instagram.com/nucleo.daguiar",
 
 ENDERECO: "Av. T-1, 1536 - Setor Bueno, Goiânia",
 
-LINK_MAPS: "https://maps.google.com"
+LINK_MAPS: "https://maps.app.goo.gl/51cjGwdhZXvykcBDA"
+
 
 };
